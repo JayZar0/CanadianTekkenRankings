c5470146fa5220fe17800cc99c06925615c78eb0
John L <lazaro4958@saskpolytech.ca>
2024-11-05 12:32:17 -0600

    added dependencies

--- src/apihandler/startgg.js
+++ src/apihandler/startgg.js
@@ -1,3 +1,18 @@
 /**
  * This file will be used to grab the information on a player using the start.gg api
  */
+
+/**
+ * This method will be used to query the players from the database
+ * the query will take:
+ *  player:
+ *      id
+ *  tournament:
+ *      id
+ *  participant:
+ *      id
+ *      placement
+ *      name
+ */
+async function queryPlayers() {
+}
